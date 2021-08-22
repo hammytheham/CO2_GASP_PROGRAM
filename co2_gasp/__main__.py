56090c786ecb7fdb8e37abfcacedbdd66b27fc49
hamish <h.alastair.r@gmail.com>
2021-08-22 06:01:39 +0200

    add files

--- co2_gasp/__main__.py
+++ co2_gasp/__main__.py
@@ -3,3 +3,17 @@ import numpy as np
 import pandas as pd
 pd.options.display.max_rows = 4000
 import sys
+
+#import modules used in code
+import data_import
+
+
+
+def main():
+    data_import.main()
+
+
+
+
+if __name__ == "__main__":
+    main()
